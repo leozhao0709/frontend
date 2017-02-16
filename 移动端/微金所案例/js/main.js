8b4d1d2c3d2690c7cbdc660972bee17a2d9fc75f
Lei Zhao <zhao434@usc.edu>
2017-02-16 08:00:07 -0800

    2017-02-16 08:00:02 auto check in

--- 移动端/微金所案例/js/main.js
+++ 移动端/微金所案例/js/main.js
@@ -2,26 +2,54 @@
 
 $(function () {
     //文档加载完成才会执行
-    function resize() {
-        var windowWidth = $(window).width();
-        var isSmallScreen = windowWidth < 768;
 
-        $('#main_ad > .carousel-inner > .item').each(function (i, item) {
-            let $item = $(item);
-            // console.log(item);
-            let imgSrc = $item.data(isSmallScreen ? 'img-xs' : 'img-lg');
-
-            $item.css('backgroundImage', `url('${imgSrc}')`);
-
-            if(isSmallScreen) {
-                $item.html(`<img src="${imgSrc}" alt="">`);
-            } 
-            else {
-                $item.empty();
-            }
-
-        })
-    }
 
     $(window).on('resize', resize).trigger('resize');
+    $('[data-toggle="tooltip"]').tooltip();
+
+    let $newsTitle = $('.news-title');
+
+    $("#news .nav-pills a").on("click", function(e){
+        let $this = $(this);
+        
+        let title = $this.data("title");
+
+        $newsTitle.text(title);
+    })
+
 })
+
+function resize() {
+    var windowWidth = $(window).width();
+    var isSmallScreen = windowWidth < 768;
+
+    $('#main_ad > .carousel-inner > .item').each(function (i, item) {
+        let $item = $(item);
+        // console.log(item);
+        let imgSrc = $item.data(isSmallScreen ? 'img-xs' : 'img-lg');
+
+        $item.css('backgroundImage', `url('${imgSrc}')`);
+
+        if (isSmallScreen) {
+            $item.html(`<img src="${imgSrc}" alt="">`);
+        } else {
+            $item.empty();
+        }
+
+    })
+
+
+    var windowWidth = $(window).width();
+
+    let $urlContainer = $('.nav-tabs');
+    let width = 50;
+
+    $urlContainer.children().each(function (index, elem) {
+        width += elem.clientWidth;
+    })
+
+    if (windowWidth < width) {
+        $urlContainer
+            .css('width', width);
+    }
+}
